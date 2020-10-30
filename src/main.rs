f4ce0ac4975051583d42fd1164d4c639e4c97e37
Fred Lahde <frederick.lahde@worldiety.de>
2020-10-30 21:48:12 +0100

    Generation function stubs, error type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod args;
+mod componentgen;
+mod error;
+mod projectgen;
 
 fn main() {
     let args = args::get_args();
-    println!("Hello, world!");
+
+    match args.subcommand_name() {
+        Some("new") => projectgen::generate(),
+        Some("component") => componentgen::generate(),
+        _ => Err(error::MineError::InvalidArgument),
+    }
+    .unwrap();
 }
---
+++ src/projectgen.rs
@@ -0,0 +1,5 @@
+use crate::error;
+
+pub fn generate() -> Result<(), error::MineError> {
+    Ok(())
+}
